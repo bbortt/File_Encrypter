679bc6d26b8ca622e0d19a34abeabaeb63978b20
Timon Borter <timon.borter@post.ch>
2016-03-10 10:36:35 +0100

    Finaly e-git works..

--- src/timon/borter/app.java
+++ src/timon/borter/app.java
@@ -22,6 +22,7 @@ import timon.borter.lib.Log;
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see http://www.gnu.org/licenses/.
  * 
+ * Contact me: timon.borter@post.ch
  * ------------------------------------------------------------------------
  * 
  * Starts a new session of FileEncrypter.
